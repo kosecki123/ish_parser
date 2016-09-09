d017407f9275181dc635929bcbd6a5e26e6bc129
Piotr Kosiński <piotrkosinski@gmail.com>
2016-09-09 13:22:36 +0200

    Updated package_dir in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='ish_parser',
       description='Parser for NOAA ISH (integrated surface hourly) reports',
       author='thayden, kosecki123',
       url='https://github.com/kosecki123/ish_parser',
-      package_dir={'': 'src'},
+      package_dir={'ish_parser': 'src'},
       py_modules=['ish_parser', 'ish_report', 'Temperature', 'Observation', 'Units', 'Components', 'Distance', 'Speed', 'Direction', 'Pressure', 'ReportType', 'Humidity'])
