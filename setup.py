a15e77f406692d0814ef8a4d63276df7b6ad92b9
Piotr Kosiński <Piotr.Kosinski@powel.com>
2016-09-12 10:47:33 +0200

    Setup.py update: set ish_parser as package location

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='ish_parser',
       description='Parser for NOAA ISH (integrated surface hourly) reports',
       author='thayden, kosecki123',
       url='https://github.com/kosecki123/ish_parser',
+      packages = ['ish_parser'],
       py_modules=['ish_parser', 'ish_report', 'Temperature', 'Observation', 'Units', 'Components', 'Distance', 'Speed', 'Direction', 'Pressure', 'ReportType', 'Humidity'])
